a37e55e2ed93f2014219dfe6bd06719dbf10fe6e
akshit <akshitgarg94670@hotmail.com>
2022-07-28 18:03:43 +0530

    reconfigure hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,24 +1,6 @@
-require("@nomiclabs/hardhat-waffle")
+require("@nomicfoundation/hardhat-toolbox");
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 1337
-    },
-//  unused configuration commented out for now
-//  mumbai: {
-//    url: "https://rpc-mumbai.maticvigil.com",
-//    accounts: [process.env.privateKey]
-//  }
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  }
-}
+  solidity: "0.8.9",
+};
